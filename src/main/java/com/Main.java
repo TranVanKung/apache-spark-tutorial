b8a115cbb3b920660136d05adf1d5acb731390f4
tranvancong <trancongtr1071997@gmail.com>
2020-06-20 01:25:35 +0700

    complete section-6

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,9 +1,9 @@
 package com;
 
+import com.google.common.collect.Iterables;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
@@ -12,22 +12,34 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> inputData = new ArrayList<>();
-        inputData.add(35);
-        inputData.add(12);
-        inputData.add(22);
-        inputData.add(32);
+        List<String> inputData = new ArrayList<>();
+        inputData.add("WARN: Tuesday 4 September 0405");
+        inputData.add("ERROR: Tuesday 4 September 0408");
+        inputData.add("FATAL: Wednesday 7 September 1632");
+        inputData.add("ERROR: Friday 7 September 1854");
+        inputData.add("WARN: Saturday 8 September 1942");
 
         Logger.getLogger("org.apache").setLevel(Level.WARN);
 
         SparkConf conf = new SparkConf().setAppName("StaringSpark").setMaster("local[*]");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        JavaRDD<Integer> originalIntegers = sc.parallelize(inputData);
-        JavaRDD<Tuple2<Integer, Double>> sqrtRdd = originalIntegers.map(
-                value -> new Tuple2<>(value, Math.sqrt(value))
-        );
-        Tuple2<Integer, Double> myValue = new Tuple2(9, 3.0);
+        // reduceByKey
+//        sc.parallelize(inputData)
+//                .mapToPair(
+//                        rawValue -> new Tuple2<>(rawValue.split("")[0], 1L)
+//                )
+//                .reduceByKey((value1, value2) -> value1 + value2)
+//                .foreach(tuple -> System.out.println(tuple._1 + " has " + tuple._2 + " instances"));
+
+        // groupBykey
+        sc.parallelize(inputData)
+                .mapToPair(
+                        rawValue -> new Tuple2<>(rawValue.split("")[0], 1L)
+                )
+                .groupByKey()
+                .foreach(tuple -> System.out.println(tuple._1 + " has " + Iterables.size(tuple._2) + " instances"));
+
 
         sc.close();
     }
