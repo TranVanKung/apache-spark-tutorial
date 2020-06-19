ddbd16925272054cac0d06be450ac5d8809a5183
tranvancong <trancongtr1071997@gmail.com>
2020-06-19 17:57:51 +0700

    initial

---
+++ src/main/java/com/Main.java
@@ -0,0 +1,28 @@
+package com;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Double> inputData = new ArrayList<>();
+        inputData.add(35.5);
+        inputData.add(12.55);
+        inputData.add(22.55);
+        inputData.add(32.55);
+
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
+
+        SparkConf conf = new SparkConf().setAppName("StaringSpark").setMaster("local[*]");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<Double> myRdd = sc.parallelize(inputData);
+        sc.close();
+    }
+}
