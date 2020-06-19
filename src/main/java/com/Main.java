19b94245eff2a196ba03bc7210cdd4bb7e7ab145
tranvancong <trancongtr1071997@gmail.com>
2020-06-20 00:22:09 +0700

    complete section-5

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -5,6 +5,7 @@ import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,23 +23,11 @@ public class Main {
         SparkConf conf = new SparkConf().setAppName("StaringSpark").setMaster("local[*]");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        JavaRDD<Integer> myRdd = sc.parallelize(inputData);
-
-        Integer result = myRdd.reduce((value1, value2) -> value1 + value2);
-
-        JavaRDD<Double> sqrtRdd = myRdd.map(value -> Math.sqrt(value));
-        sqrtRdd.collect().forEach(System.out::println);
-
-        System.out.println(result);
-
-        // how many elements in sqrtRdd
-        System.out.println(sqrtRdd.count());
-
-        // using map and reduce for counting
-        JavaRDD<Integer> singleIntegerRdd = sqrtRdd.map(value -> 1);
-        Integer count = singleIntegerRdd.reduce((value1, value2) -> value1 + value2);
-
-        System.out.println("count: " + count);
+        JavaRDD<Integer> originalIntegers = sc.parallelize(inputData);
+        JavaRDD<Tuple2<Integer, Double>> sqrtRdd = originalIntegers.map(
+                value -> new Tuple2<>(value, Math.sqrt(value))
+        );
+        Tuple2<Integer, Double> myValue = new Tuple2(9, 3.0);
 
         sc.close();
     }
