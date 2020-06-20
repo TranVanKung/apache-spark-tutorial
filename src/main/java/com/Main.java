0e121ceec83d0e4bf8383a51d2599c2aa9897097
tranvancong <trancongtr1071997@gmail.com>
2020-06-20 17:32:45 +0700

    complete section-8

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -12,21 +12,15 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> inputData = new ArrayList<>();
-        inputData.add("WARN: Tuesday 4 September 0405");
-        inputData.add("ERROR: Tuesday 4 September 0408");
-        inputData.add("FATAL: Wednesday 7 September 1632");
-        inputData.add("ERROR: Friday 7 September 1854");
-        inputData.add("WARN: Saturday 8 September 1942");
-
         Logger.getLogger("org.apache").setLevel(Level.WARN);
 
         SparkConf conf = new SparkConf().setAppName("StaringSpark").setMaster("local[*]");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        sc.parallelize(inputData)
+        JavaRDD<String> initialRdd = sc.textFile("src/main/resources/subtitles/input.txt");
+
+        initialRdd
                 .flatMap(value -> Arrays.asList(value.split(" ")).iterator())
-                .filter(word -> word.length() > 1)
                 .foreach(word -> System.out.println(word));
 
         sc.close();
